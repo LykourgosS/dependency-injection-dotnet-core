a11b2cde8c315f5a9969041a9a76751fadaf786a
LykourgosS <lpsarantidis@gmail.com>
2024-05-30 15:43:00 +0300

    feat: Depending on the abstractions
    make OrderSender and OrderManager to implement corresponding interfaces
    and depend on them and not in their implementations (concrete classes)

--- OrderManagementWeb/Managers/OrderManager.cs
+++ OrderManagementWeb/Managers/OrderManager.cs
@@ -1,15 +1,21 @@
 ﻿using System.Threading.Tasks;
+using OrderManagement.Interfaces;
 using OrderManagement.Models;
 
 namespace OrderManagement.Managers
 {
-    public class OrderManager
+    public class OrderManager : IOrderManager
     {
+        private IOrderSender sender;
+
+        public OrderManager(IOrderSender sender)
+        {
+            this.sender = sender;
+        }
+
         public async Task<string> Transmit(Order order)
         {
-            var orderSender = new OrderSender();
-
-            return await orderSender.Send(order);
+            return await sender.Send(order);
         }
     }
 }
