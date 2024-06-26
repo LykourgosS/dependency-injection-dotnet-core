dc7463c5ca9762eb69d09b7b6ceebbc047f6da03
LykourgosS <lpsarantidis@gmail.com>
2024-06-26 21:41:46 +0300

    feat: Registering the dependencies
    Scoped: the scoped lifetime allows you to create an instance of a service for each client request. This is particularly useful in the ASP.NET context since it allows you to share the same service instance for the duration of an HTTP request processing. To enable the scoped lifetime, you need to use the AddScoped() method.

--- OrderManagementWeb/Startup.cs
+++ OrderManagementWeb/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using OrderManagement.Interfaces;
+using OrderManagement.Managers;
 
 namespace OrderManagement
 {
@@ -26,6 +21,9 @@ namespace OrderManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<IOrderSender, HttpOrderSender>();
+            services.AddScoped<IOrderManager, OrderManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
