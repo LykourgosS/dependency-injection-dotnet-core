3bc00304b7ae8fb47b6e523095333a2eb4d75c68
LykourgosS <lpsarantidis@gmail.com>
2024-07-03 11:54:33 +0300

    feat: Copy Interfaces, Managers, and Models folders

---
+++ OrderManagementConsole/Managers/OrderManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using OrderManagement.Interfaces;
+using OrderManagement.Models;
+
+namespace OrderManagement.Managers
+{
+    public class OrderManager : IOrderManager
+    {
+        private IOrderSender sender;
+
+        public OrderManager(IOrderSender sender)
+        {
+            this.sender = sender;
+        }
+
+        public async Task<string> Transmit(Order order)
+        {
+            return await sender.Send(order);
+        }
+    }
+}
