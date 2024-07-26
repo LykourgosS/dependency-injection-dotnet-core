50b7f5a8a31d5032c7ed8e190728767947fdfdc6
LykourgosS <lpsarantidis@gmail.com>
2024-07-26 13:31:58 +0300

    feat: Setup Program.cs file (configure services etc)

--- OrderManagementConsole/Program.cs
+++ OrderManagementConsole/Program.cs
@@ -1,9 +1,50 @@
-﻿namespace OrderManagementConsole;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderManagement.Interfaces;
+using OrderManagement.Managers;
+using OrderManagement.Models;
+using System.Runtime.CompilerServices;
+
+namespace OrderManagementConsole;
 
 class Program
 {
+    private static IServiceProvider? serviceProvider;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ConfigureServices();
+
+        var orderManager = serviceProvider?.GetService<OrderManager>();
+        var order = CreateOrder();
+
+        orderManager?.Transmit(order);
+    }
+
+    private static void ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        services.AddTransient<IOrderSender, HttpOrderSender>();
+        services.AddTransient<IOrderManager, OrderManager>();
+
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    private static Order CreateOrder()
+    {
+        return new Order
+        {
+            CustomerId = "12345",
+            Date = new DateTime(),
+            TotalAmount = 145,
+            Items = new List<OrderItem>
+                {
+                    new OrderItem {
+                        ItemId = "99999",
+                        Quantity = 1,
+                        Price = 145
+                    }
+                }
+        };
     }
 }
