1cbfb9266f4e387d920746b5caaa39f5e4bd8400
LykourgosS <lpsarantidis@gmail.com>
2024-07-26 15:59:49 +0300

    feat: Register IManagementConnection dependency
    using the AddSingleton() method is similar to registering a service with
    Add() and a service instance as an argument. However, the AddSingleton()
    method grants you that the instance creation of the service will be delayed
    until the first time it will be requested in the application

--- OrderManagementWeb/Startup.cs
+++ OrderManagementWeb/Startup.cs
@@ -1,3 +1,4 @@
+using Auth0.ManagementApi;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +25,8 @@ namespace OrderManagement
 
             services.AddScoped<IOrderSender, HttpOrderSender>();
             services.AddScoped<IOrderManager, OrderManager>();
+
+            services.AddSingleton<IManagementConnection, HttpClientManagementConnection>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
